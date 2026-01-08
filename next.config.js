0e3cbd6f7b415c3d07b6b2b914da6dac35a0c784
mjmilne1 <mjmilne@proton.me>
2026-01-08 13:05:19 +1100

    build: enable static export for demo builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
