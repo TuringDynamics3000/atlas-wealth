017aa546fedf722a98ee774ba581e1ed38a499bd
mjmilne1 <mjmilne@proton.me>
2026-01-10 08:22:53 +1100

    chore: revert nav change from adviser PR

--- components/layout/nav.ts
+++ components/layout/nav.ts
@@ -9,10 +9,3 @@ export const NAV_ITEMS: NavItem[] = [
   { label: 'Portfolios', href: '/wealth/portfolios' },
   { label: 'Reporting',  href: '/wealth/reporting' },
 ]
-/* Adviser UX added */
-<li>
-  <a href="/adviser" className="hover:underline">Adviser Dashboard</a>
-</li>
-<li>
-  <a href="/adviser/clients" className="hover:underline">Clients</a>
-</li>
